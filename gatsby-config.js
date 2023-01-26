0bc8cc5bbdc9e52268dc46d61a16de0765511724
Macky Beltran <macky@kearnstechnology.com>
2023-01-26 16:40:02 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `magicmarkv2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
